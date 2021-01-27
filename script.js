c490385be1037d7a8de7e881a4e5a44b5067950e
Nayemul <nayemulhasan2016@gmail.com>
2021-01-28 03:31:05 +0600

    decrease qty and price by click - sign

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//increase value for phone and phone cases
 document.getElementById('phone-increase').addEventListener('click', function(){
     const phoneInput = document.getElementById('phone-count');
     const phoneCount = parseInt(phoneInput.value);
@@ -9,13 +9,32 @@ document.getElementById('phone-increase').addEventListener('click', function(){
     document.getElementById('phone-total').innerText = '$'+ phoneTotal;
     console.log(phoneInput.value);
 })
-
 document.getElementById('case-increase').addEventListener('click', function(){
     const caseInput = document.getElementById('case-count');
     const caseCount = parseInt(caseInput.value);
     const caseNewCount = caseCount + 1;
     caseInput.value = caseNewCount;
 
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText = '$' + caseTotal;
+})
+//decrease value for phone and phone case
+
+document.getElementById('phone-decrease').addEventListener('click', function(){
+    const phoneInput = document.getElementById('phone-count');
+    const phoneCount = parseInt(phoneInput.value);
+    const phoneNewCount = phoneCount - 1;
+    phoneInput.value = phoneNewCount;
+
+    const phoneTotal = phoneNewCount * 1219;
+    document.getElementById('phone-total').innerText = '$' + phoneTotal;
+})
+
+document.getElementById('case-decrease').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount - 1;
+    caseInput.value = caseNewCount;
     const caseTotal = caseNewCount * 59;
     document.getElementById('case-total').innerText = '$' + caseTotal;
 })
