1099722e0cecf84a8177708dcc04b1a86857d3f9
Nayemul <nayemulhasan2016@gmail.com>
2021-01-28 02:58:19 +0600

    increase product and price by click + sign

---
+++ script.js
@@ -0,0 +1,21 @@
+
+document.getElementById('phone-increase').addEventListener('click', function(){
+    const phoneInput = document.getElementById('phone-count');
+    const phoneCount = parseInt(phoneInput.value);
+    const phoneNewCount = phoneCount + 1;
+
+    phoneInput.value = phoneNewCount;
+    const phoneTotal = phoneNewCount * 1219;
+    document.getElementById('phone-total').innerText = '$'+ phoneTotal;
+    console.log(phoneInput.value);
+})
+
+document.getElementById('case-increase').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount + 1;
+    caseInput.value = caseNewCount;
+
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText = '$' + caseTotal;
+})
